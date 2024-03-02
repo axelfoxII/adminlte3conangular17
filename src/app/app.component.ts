cc4d2e962574e7882a3c467107929c29882b8741
Alex Salvatierra <asfoxII@gmail.com>
2024-03-01 22:38:14 -0400

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'adminlet3Angular17';
+}
