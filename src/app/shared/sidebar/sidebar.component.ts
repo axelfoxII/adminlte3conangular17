9c7b047c1a70b0d223fd0693038c3679dcd1763f
Alex Salvatierra <asfoxII@gmail.com>
2024-09-25 21:26:05 -0400

    Update Sidebar

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -3,33 +3,41 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.css'
+  styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent {
+  nombreUsuario = localStorage.getItem('nombre');
 
-  nombreUsuario= localStorage.getItem('nombre');
+  public menuItems: any[] = [
+    {
+      titulo: 'Dashboard',
+      icono: 'nav-icon fas fa-tachometer-alt',
+      submenu: [
+        { titulo: 'DataTable', url: 'heroes', icon: 'far fa-circle' },
+        { titulo: 'Toaster', url: 'toaster', icon: 'far fa-circle' },
+        {
+          titulo: 'Advanced',
+          icon: 'far fa-circle',
+          submenu: [ // Submenú dentro de otro submenú
+            { titulo: 'Settings', url: 'advanced/settings', icon: 'far fa-dot-circle' },
+            { titulo: 'Logs', url: 'advanced/logs', icon: 'far fa-dot-circle' },
+          ]
+        },
+      ]
+    },
+    {
+      titulo: 'Settings', // Menú adicional
+      icono: 'nav-icon fas fa-cogs',
+      submenu: [
+        { titulo: 'Profile', url: 'profile', icon: 'far fa-user' },
+        { titulo: 'Privacy', url: 'privacy', icon: 'fa fa-lock' },
+      ]
+    }
+  ];
 
-  public menuItems:any[]=[{
-
-    titulo:'Dashboard',
-    icono:'nav-icon fas fa-tachometer-alt',
-    submenu:[
-      {titulo:'DataTable', url:'heroes', icon:'far fa-circle' },
-      {titulo:'Toaster', url:'toaster', icon:'far fa-circle' },
-    ]
-
-
-  }];
-
-  logout(){
-
+  logout() {
     localStorage.removeItem('usuario');
     localStorage.removeItem('nombre');
-
-    location.href='login';
-
+    location.href = 'login';
   }
-
-
-
 }
